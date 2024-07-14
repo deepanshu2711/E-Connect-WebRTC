b0f366551af3ed1a58940d060fb2c44480827e3d
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-07-15 01:39:43 +0530

    initialize backend and frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-3xl text-rose-500 font-semibold">Home screen</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
